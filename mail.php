51d06b0cff45c64f144a4eabf20f6b059881e191
zaaaaid <102842448+zaaaaid@users.noreply.github.com>
2023-04-25 13:36:49 +0800

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
-$recipient = "example@gmail.com";
+$recipient = "sisigrice16@gmail.com";
 
 mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
